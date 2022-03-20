8f958df3ed47bf59f35d0251a041481466e6788a
akhil raja rao <akhilr37.gmail.com>
2022-03-20 14:45:01 +0000

    added all tables interface

--- src/components/MoneyManager/index.js
+++ src/components/MoneyManager/index.js
@@ -27,6 +27,52 @@ class MoneyManager extends Component {
           <div className="moneyDetailsContainer">
             <MoneyDetails />
           </div>
+          <div className="tables">
+            <div className="leftTableContainer">
+              <form className="leftTable">
+                <h1 className="table-Heading">Add Transaction</h1>
+                <label className="labelCss" htmlFor="title">
+                  TITLE
+                </label>
+                <input
+                  className="inputcss"
+                  type="text"
+                  placeholder="TITLE"
+                  id="title"
+                />
+                <label className="labelCss" htmlFor="amount">
+                  AMOUNT
+                </label>
+                <input
+                  className="inputcss"
+                  type="text"
+                  placeholder="TITLE"
+                  id="amount"
+                />
+                <label className="labelCss" htmlFor="options">
+                  TYPE
+                </label>
+                <select id="options" className="inputcss">
+                  {transactionTypeOptions.map(item => (
+                    <option className="options" value={item.optionIds}>
+                      {item.displayText}
+                    </option>
+                  ))}
+                </select>
+                <button className="buttonAdd" type="submit">
+                  Add
+                </button>
+              </form>
+            </div>
+            <div className="rightTableContainer">
+              <h1 className="historyCss">History</h1>
+              <div className="historyContainer">
+                <h1 className="Hitems">Title</h1>
+                <h1 className="Hitems">Amount</h1>
+                <h1 className="Hitems">Type</h1>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
